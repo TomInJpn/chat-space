acd48b1ec0b8e8b95fd4aac07fb1d41e0a2421f8
TomInJpn <66042540+TomInJpn@users.noreply.github.com>
2020-07-01 19:42:40 +0900

    デプロイの自動化終了03

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,13 @@
-# config valid for current version and patch releases of Capistrano
-lock "3.14.1"
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.14.1'
 
-set :application, "chat-space"
-set :repo_url, "git@github.com:TomInJpn/chat-space.git"
+# Capistranoのログの表示に利用する
+set :application, 'chat-space'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:TomInJpn/chat-space.git'
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
